1a4f81aed58ef5f15748d093a4e52aefb245d0ba
Unknown <josecarlos.pimienta@gmail.com>
2018-11-25 18:28:41 -0700

    primera version de tabla y conexion a DB
    estos son los primeros files en los que se tiene como hacer una tabla en php y en conexiones.php se tiene como hacer la conexxion a la base de datos

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php
+ echo 'Hola Mundo' ;
+
+ echo "<div class='container-fluid' style='padding-top:15px;'>
+             <div class='panel panel-info'>
+             <div class='panel-heading' style='text-align:center;'>";
+
+ echo "<table border='1' id='tablaUS' class='table table-striped table-hover table-bordered'>
+                <thead>
+                    <th>Nombre</th>
+                    <th>Apellido Paterno</th>
+                    <th>Apellido Materno</th>
+                    <th>Institucion</th>
+                    <th>Tipo</th>
+                    <th>Folio</th>
+                    <th>Verificacion</th>
+                </thead>";
+
+                echo "<tr>
+                        <td>jj</td>
+                        <td>pp</td>
+                        <td>ii</td>
+                        <td>us</td>
+                        <td>al</td>
+                        <td>023</td>
+                        <td>asd</td>
+                    </tr>";
+echo"</table>";
+ ?>
+ </body>
+</html>
