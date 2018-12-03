93f037c02c62b27ef5d3ebfeab75d60e8e3442e0
Unknown <josecarlos.pimienta@gmail.com>
2018-12-02 17:30:36 -0700

    ya verifica llamadas antes de eliminar cliente
    ya se agrago que verifique si un cliente ralizo llamadas antes de eliminarlo

--- D_cliente.php
+++ D_cliente.php
@@ -1,14 +1,30 @@
 <?php
 //aqui agrega un nuevo cliente a la bd
 
-$nif = $_GET['id'];
+$id = $_GET['id'];
 
 
 include 'conexion.php';//abre concexion con mysql
 
+$tabla=$mysqli->query("select ll.num_destino, ll.num_saliente from
+                      llamada ll join (select li.id_linea id_li from
+                      linea li where
+                      li.id_cliente = ".$id. ") NTabla
+                      on ll.num_destino=NTabla.id_li or num_saliente=NTabla.id_li; ");
 
-$tabla=$mysqli->query("delete from cliente where nif='".$nif."'");
-
-$mysqli->close();//cierra la conexion de mysql
-header('Location: mostrar_clientes.php');
+if($tabla->num_rows == 0){
+  $tabla=$mysqli->query("delete from cliente where id_cliente=".$id."");
+  $mysqli->close();
+  echo "<script>
+        alert('El cliente ha sido eliminado exitosamente');
+        window.location.href='mostrar_clientes.php';
+        </script>";
+  //header('Location: mostrar_clientes.php');
+}else {
+  $mysqli->close();
+  echo "<script>
+        alert('El cliente no se puede eliminar ya que tiene llamadas registradas');
+        window.location.href='mostrar_clientes.php';
+        </script>";
+}
 ?>
