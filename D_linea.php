37259cb50118c25a20d1469aafa028fcecec43e5
Unknown <josecarlos.pimienta@gmail.com>
2018-12-03 17:18:16 -0700

    arreglado redireccionamiento
    se elimino el redireccionamiento que tenia previamente.
    Ahora se espera a que se confirme la ventana de ok

--- D_linea.php
+++ D_linea.php
@@ -1,14 +1,31 @@
 <?php
 //aqui agrega un nuevo cliente a la bd
 
-$num = $_GET['id'];
+$id = $_GET['id'];
 
 
 include 'conexion.php';//abre concexion con mysql
 
+$tabla=$mysqli->query("select distinct ll.num_destino, ll.num_saliente from
+                      llamada ll join linea l
+                      on ll.num_destino=".$id." or num_saliente=".$id."; ");
 
-$tabla=$mysqli->query("delete from linea where num_telefono='".$num."'");
+if($tabla->num_rows == 0){
+    $tabla=$mysqli->query("delete from linea where id_linea=".$id."");
+    $mysqli->close();
+    echo "<script>
+          alert('La linea ha sido eliminado exitosamente');
+          window.location.href='mostrar_linea.php';
+          </script>";
 
-$mysqli->close();//cierra la conexion de mysql
-header('Location: mostrar_linea.php');
+}else {
+    $mysqli->close();
+    echo "<script>
+          alert('La linea no puede ser eliminar ya que tiene llamadas registradas');
+          window.location.href='mostrar_linea.php';
+          </script>";
+}
+
+
+//$tabla=$mysqli->query("delete from linea where num_telefono='".$num."'");
 ?>
