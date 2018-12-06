cfc1a4b27114c60ffbda660baf51a339921ef023
Unknown <josecarlos.pimienta@gmail.com>
2018-12-06 13:21:22 -0700

    terminado lo de llamadas falta arreglar algo de pdf
    asmdnfdjndfja

--- mostrar_llamada.php
+++ mostrar_llamada.php
@@ -1,86 +1,122 @@
-<html>
- <head>
-  <title>Llamadas</title>
- </head>
- <body>
- <?php
+<!DOCTYPE html>
+<?php
+$id = $_GET["id"];
+include 'conexion.php';
 
-    $id = $_GET["id"];
-    include 'conexion.php';
+//PARA PONER EL TITULO DE QUE NUMEOR LLAMO
+$num=$mysqli->query("select l.num_telefono from linea l where l.id_linea=".$id.";")->fetch_object()->num_telefono;
+?>
+<html lang="en">
+<head>
+    <title>Compañia Telefonica</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 
-    //PARA PONER EL TITULO DE QUE NUMEOR LLAMO
-    $num=$mysqli->query("select l.num_telefono from linea l where l.id_linea=".$id.";")->fetch_object()->num_telefono;
-    //$num = $tabla->fetch_object()->num_telefono;
-    echo "<h1> historial de llamadas del numero: ".$num.".</h1>";
+<body>
 
-    ///////////////////////////////////////////////
-    //PARA LLAMADAS REALIZADAS POR EL NUMERO   ////
-    ///////////////////////////////////////////////
-    //$tabla=$mysqli->query("select num_destino, costo from llamada where num_saliente='".$id."';");
-    $tabla=$mysqli->query("select l.num_telefono, t.costo
-                           from linea l join (select ll.num_destino nd, ll.costo costo from
-                           llamada ll where ll.num_saliente=".$id.") t
-                           on l.id_linea=t.nd;");
-    //mysqli_close($mysqli);
+    <!--  barra  -->
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="index.php">Compañia Telefonica</a>
+            </div>
+        </div>
+    </nav>
 
-    //echo "<h1> historial de llamadas del numero: ".$num_salida.".</h1>";
+    <div class="container-fluid text-center">
+        <div class="row content">
+            <!-- Derecha -->
+            <div class="col-sm-2 sidenav">
+                <p><a href="ingresar_cliente.php"class="btn btn-success">Ingresar Cliente</a></p>
+                <p><a href="ingresar_linea.php" class="btn btn-success">Ingresar Linea</a></p>
+                <p><a href="index.php" class="btn btn-info">Lista de Clientes</a></p>
+                <p><a href="index.php" class="btn btn-info">Lista de Clientes</a></p>
+            </div>
+            <!--  Centro -->
+        <div class="col-sm-8 text-left">
+          <?php echo "<h1>Historial de Llamadas de: ".$num."</h1>"; ?>
 
-    if ($tabla->num_rows > 0) {
-      echo "<h2> Llamadas realizadas.</h2>";
-      echo "<table border='1' id='tablaUS' class='table table-striped table-hover table-bordered'>
-        <thead>
-          <th>Numero Destino</th>
-          <th>Costo</th>
-        </thead>";
+            <!-- <p> Cliente ID</p> -->
+            <hr>
+            <h2> Llamadas realizadas.</h2>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Numero Destino</th>
+                        <th>Costo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        //include 'conexion.php';
+                        $tabla=$mysqli->query("select l.num_telefono, t.costo
+                                               from linea l join (select ll.num_destino nd, ll.costo costo from
+                                               llamada ll where ll.num_saliente=".$id.") t
+                                               on l.id_linea=t.nd;");
+                        //mysqli_close($mysqli);
 
-        while($row = $tabla->fetch_assoc()) {
-          echo "<tr>
-            <td>".$row['num_telefono']."</td>
-            <td>".$row['costo']."</td>
+                        if ($tabla->num_rows > 0) {
+                              while($row = $tabla->fetch_assoc()) {
+                                echo "<tr>
+                                  <td>".$row['num_telefono']."</td>
+                                  <td>".$row['costo']."</td>
 
-          </tr>";
-        }
-        echo"</table>";
-    }
-    else {
-      echo "<h2> Llamadas realizadas.</h2>";
-      echo "<p> No hay registro de llamadas realizadas.</p>";
-    }
+                                </tr>";
+                              }
+                              echo"</table>";
+                        }
+                        else {
+                            echo "<p> No hay registro de llamadas realizadas.</p>";
+                        }
+                    ?>
+                </tbody>
+            </table>
 
-    ///////////////////////////////////////////
-    //AHORA PARA LAS LLAMADAS RECIBIDAS  //////
-    ///////////////////////////////////////////
-    //$tabla=$mysqli->query("select num_saliente, costo from llamada where num_destino='".$id."';");
-    $tabla=$mysqli->query("select l.num_telefono, t.costo
-                           from linea l join (select ll.num_saliente ns, ll.costo costo from
-                           llamada ll where ll.num_destino=".$id.") t
-                           on l.id_linea=t.ns;");
-    mysqli_close($mysqli);
+            <hr>
+            <h2> Llamadas realizadas.</h2>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Numero Entrante</th>
+                        <th>Costo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        //include 'conexion.php';
+                        $tabla=$mysqli->query("select l.num_telefono, t.costo
+                                               from linea l join (select ll.num_saliente ns, ll.costo costo from
+                                               llamada ll where ll.num_destino=".$id.") t
+                                               on l.id_linea=t.ns;");
+                        mysqli_close($mysqli);
 
-    if ($tabla->num_rows > 0) {
-      echo "<h2> Llamadas recibidas.</h2>";
-      echo "<table border='1' id='tablaUS' class='table table-striped table-hover table-bordered'>
-        <thead>
-          <th>Numero Saliente</th>
-          <th>Costo</th>
-        </thead>";
+                        if ($tabla->num_rows > 0) {
+                              while($row = $tabla->fetch_assoc()) {
+                                echo "<tr>
+                                  <td>".$row['num_telefono']."</td>
+                                  <td>".$row['costo']."</td>
 
-        while($row = $tabla->fetch_assoc()) {
-          echo "<tr>
-            <td>".$row['num_telefono']."</td>
-            <td>".$row['costo']."</td>
+                                </tr>";
+                              }
+                              echo"</table>";
+                        }
+                        else {
+                            echo "<p> No hay registro de llamadas realizadas.</p>";
+                        }
+                    ?>
+                </tbody>
+            </table>
 
-          </tr>";
-        }
-        echo"</table>";
-    }
-    else {
-      echo "<h2> Llamadas realizadas.</h2>";
-      echo "<p> No hay registro de llamadas recibidas.</p>";
-    }
+            <p><a href="LlamadasPDF.php?id=<?php echo $id; ?>" class="btn btn-info">PDF</a></p>
+        </div>
+    </div>
+<footer class="container-fluid text-center">
+  <p>Universidad de Sonora</p>
+</footer>
 
-    echo '<a href="mostrar_linea.php">Volver</a>';
-    //<td><a href='D_Linea.php?id=".$row['num_telefono']."' alt='edit'>Eliminar</a></td> esta linea va bajo la linea 27 en caso de querer agragr un boton de eliminar
- ?>
 </body>
 </html>
